bb28a68f96cad7c4197d205fb7aff65e261b1479
PsyTheGuy <psyplaysgames@gmail.com>
2022-07-31 22:28:48 +0200

    solved exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    const toRemove = [...args];
+    for (let arg of toRemove) {
+        arr = arr.filter(item => item !== arg);
+    };
+    return arr;
 };
 
 // Do not edit below this line
