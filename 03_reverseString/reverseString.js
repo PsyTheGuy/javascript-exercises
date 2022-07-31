b905f52729feaf83c97b67cc15a0cf5d5e0abf22
PsyTheGuy <psyplaysgames@gmail.com>
2022-07-31 21:27:53 +0200

    solved exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(stringToReverse) {
+    let charList = [...stringToReverse];
+    let reversedCharList = [];
 
+    for (char of charList) {
+        reversedCharList.unshift(char);
+    };
+
+    return reversedCharList.join('');
 };
 
 // Do not edit below this line
