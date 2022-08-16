318f903e3f04b6ba999d64fd0cd34f95ee603acf
PsyTheGuy <psyplaysgames@gmail.com>
2022-08-17 00:04:25 +0200

    solved exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  let oldestAge = -Infinity;
+  let oldestPerson = null;
+  for (person of people) {
+    if (!person.yearOfDeath) {
+      person.yearOfDeath = new Date().getFullYear();
+    };
+    if (person.yearOfDeath - person.yearOfBirth > oldestAge) {
+      oldestAge = person.yearOfDeath - person.yearOfBirth;
+      oldestPerson = person
+    };
+  };
+  return oldestPerson;
 };
 
 // Do not edit below this line
