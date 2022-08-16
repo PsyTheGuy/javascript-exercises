3b7fd15104981be56e8d9bc98dd17abfc8afb9bd
PsyTheGuy <psyplaysgames@gmail.com>
2022-08-16 21:13:42 +0200

    solved exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	return a.reduce((sum, num) => sum + num,0);
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  return a.reduce((multi, num) => (a.length > 0) ? multi * num : 0, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  (a > 0) ? result = [...Array(a+1).keys()].slice(1).reduce((multi, num) => multi * num, 1) : result = 1;
+  return result;
 };
 
 // Do not edit below this line
