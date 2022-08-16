63cb3005f3a44c9210bf9f4756d0fbe4895cc7d8
PsyTheGuy <psyplaysgames@gmail.com>
2022-08-16 23:31:50 +0200

    solved exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+  let string = text.split(' ').map(word => word.replace(/\W/g, ' ').trim().toLowerCase()).join('');
+  for (let i = 0; i < Math.floor(string.length / 2); i++) {
+    if (string.charAt(i) === string.charAt(string.length-1-i)) {
+      continue;
+    } else {
+      return false;
+    };
+  };
+  return true;
 };
 
 // Do not edit below this line
